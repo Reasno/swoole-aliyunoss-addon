518575e697302842105d1774b219ebed17df345f
Reasno <guxi99@gmail.com>
2020-05-15 10:55:08 +0800

    fix bitwise operator

--- src/Hook.php
+++ src/Hook.php
@@ -3,7 +3,7 @@
 namespace Oss\OssClient {
     function is_resource($resource)
     {
-        if (\Swoole\Runtime::getHookFlags() ^ SWOOLE_HOOK_CURL) {
+        if (\Swoole\Runtime::getHookFlags() & SWOOLE_HOOK_CURL) {
             return \is_resource($resource) || $resource instanceof \Swoole\Curl\Handler;
         }
         return \is_resource($resource);
@@ -13,7 +13,7 @@ namespace Oss\OssClient {
 namespace Oss\Http {
     function is_resource($resource)
     {
-        if (\Swoole\Runtime::getHookFlags() ^ SWOOLE_HOOK_CURL) {
+        if (\Swoole\Runtime::getHookFlags() & SWOOLE_HOOK_CURL) {
             return \is_resource($resource) || $resource instanceof \Swoole\Curl\Handler;
         }
         return \is_resource($resource);
