6f25ef8ab307a02faf3f89d28c09b19e8c5b3e8b
Reasno <guxi99@gmail.com>
2020-05-15 10:16:31 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+本组件解决了阿里云官方PHP OSS SDK和Swoole CURL_HOOK的兼容问题。
+
+安装本组件后就可以顺畅开启CURL_HOOK协程化阿里云OSS。
+
+## 问题
+开启CURL_HOOK后使用阿里云OSS会报错：
+
+```
+Oss\Core\OssException: : RequestId:
+```
+
+
+## 解决方案
+请同时安装官方SDK和本组件。
+
+```bash
+composer require aliyuncs/oss-sdk-php
+composer require reasno/swoole-aliyunoss-addon
+```
---
+++ src/Hook.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Oss\OssClient {
+    function is_resource($resource)
+    {
+        if (\Swoole\Runtime::getHookFlags() ^ SWOOLE_HOOK_CURL) {
+            return \is_resource($resource) || $resource instanceof \Swoole\Curl\Handler;
+        }
+        return \is_resource($resource);
+    }
+}
+
+namespace Oss\Http {
+    function is_resource($resource)
+    {
+        if (\Swoole\Runtime::getHookFlags() ^ SWOOLE_HOOK_CURL) {
+            return \is_resource($resource) || $resource instanceof \Swoole\Curl\Handler;
+        }
+        return \is_resource($resource);
+    }
+}
